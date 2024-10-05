5f37066e2ba83d21fd3bf3d142b5c0178fa83e86
hayk111 <hayk.atoyan111@gmail.com>
2024-10-05 16:08:06 +0400

    changes vars

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+const API_URL = import.meta.env.VITE_API_URL;
 console.log('🚀 ~ API_URL:', API_URL);
 
 export const fetchBalanceAPI = async (address) => {
